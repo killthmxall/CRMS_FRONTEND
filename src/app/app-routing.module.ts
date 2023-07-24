30cf40b428b159973e12d04ddae3981b93b102a7
JEDAV01 <136944117+JEDAV01@users.noreply.github.com>
2023-07-23 21:31:02 -0500

    Creacion formulario Paciente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { PacienteComponent } from './components/paciente/paciente.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'paciente', component: PacienteComponent }
+  
 ];
 
 @NgModule({
